b16785930c2e92f2df79e6a9a4b90d5a7436814d
tim <tim@twa.ninja>
2021-10-03 09:16:44 -0600

    Initial project setup: compiles, flashes & blinks

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+#![no_std]
+#![no_main]
+
+use bl602_hal as hal;
+use core::fmt::Write;
+use hal::{
+    clock::{Strict, SysclkFreq, UART_PLL_FREQ},
+    pac,
+    prelude::*,
+    serial::*,
+};
+use embedded_hal::delay::blocking::DelayMs;
+use embedded_hal::digital::blocking::OutputPin;
+use panic_halt as _;
+
+#[riscv_rt::entry]
+fn main() -> ! {
+    let dp = pac::Peripherals::take().unwrap();
+    let mut parts = dp.GLB.split();
+
+    // Set up all the clocks we need
+    let clocks = Strict::new()
+        .use_pll(40_000_000u32.Hz())
+        .sys_clk(SysclkFreq::Pll160Mhz)
+        .uart_clk(UART_PLL_FREQ.Hz())
+        .freeze(&mut parts.clk_cfg);
+
+    // Set up uart output. Since this microcontroller has a pin matrix,
+    // we need to set up both the pins and the muxs
+    let pin16 = parts.pin16.into_uart_sig0();
+    let pin7 = parts.pin7.into_uart_sig7();
+    let mux0 = parts.uart_mux0.into_uart0_tx();
+    let mux7 = parts.uart_mux7.into_uart0_rx();
+
+    // Configure our UART to 2MBaud, and use the pins we configured above
+    let mut serial = Serial::uart0(
+        dp.UART,
+        Config::default().baudrate(2_000_000.Bd()),
+        ((pin16, mux0), (pin7, mux7)),
+        clocks,
+    );
+    // Also set up a pin as GPIO, to blink an LED
+    let mut gpio5 = parts.pin5.into_pull_down_output();
+
+    // Create a blocking delay function based on the current cpu frequency
+    let mut d = bl602_hal::delay::McycleDelay::new(clocks.sysclk().0);
+
+    loop {
+        // Toggle the LED on and off once a second. Report LED status over UART
+        gpio5.set_high().unwrap();
+        serial.write_str("LEDs on\r\n").ok();
+        d.delay_ms(1000).unwrap();
+
+        gpio5.set_low().unwrap();
+        serial.write_str("LEDs off\r\n").ok();
+        d.delay_ms(1000).unwrap();
+    }
+}
